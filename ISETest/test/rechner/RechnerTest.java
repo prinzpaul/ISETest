b02b881f31f5daabab570025742d7b721dc30ee9
paul <paul@192.168.99.64>
2018-11-14 11:43:19 +0100

    t

--- ISETest/test/rechner/RechnerTest.java
+++ ISETest/test/rechner/RechnerTest.java
@@ -10,17 +10,17 @@ class RechnerTest {
 	@Test
 	public void testrechnen(){
 		Rechner hi = new Rechner();
-		assertequals("Test", 4, hi.addiere(1,3));
+		assertEquals(4, hi.addiere(1,3));
 	}
 	@Test
 	public void testrechnerr(){
 		Rechner hi = new Rechner();
-		assertequals("Test", 120, hi.faku(5));
+		assertEquals(120, hi.faku(5));
 	}
 	@Test
 	public void testrechne(){
 		Rechner hi = new Rechner();
-		assertequals("Test", -2, hi.sub(1,3));
+		assertEquals(-2, hi.sub(1,3));
 	}
 
 }
